66f4961eff34b7211faac5f421f8809b197dfb82
Richard Basdeo <basdeorichard@gmail.com>
2021-03-15 17:44:12 -0400

    Setup decibel viewController with UI elements and animations - Richard

--- Insight Security/Controller/DecibelsViewController.swift
+++ Insight Security/Controller/DecibelsViewController.swift
@@ -9,10 +9,51 @@ import UIKit
 
 class DecibelsViewController: UIViewController {
 
+
+    
+    @IBOutlet weak var securityButton: UIButton!
+    @IBOutlet weak var circleImage: UIImageView!
+    @IBOutlet weak var decibelsLabel: UILabel!
+    
+    var continueAnimation = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.9450049996, green: 0.9451631904, blue: 0.9449841976, alpha: 1)
+        circleImage.isHidden = true
+        
     }
-
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        UIView.animate(withDuration: 2) {
+            self.view.backgroundColor = .black
+            self.circleImage.isHidden = false
+        }
+        view.backgroundColor = .black
+        securityButton.setTitle("Security Mode Started", for: .normal)
+        decibelsLabel.text = "Calculating the average decibels of the room ....."
+        circleAnimation()
+    }
+    
+    func circleAnimation (){
+        
+        UIView.animate(withDuration: 3) {
+            self.circleImage.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
+        } completion: { (_) in
+            
+            UIView.animate(withDuration: 3) {
+                self.circleImage.transform = CGAffineTransform(scaleX: 2, y: 2)
+            }completion: { (_) in
+                UIView.animate(withDuration: 3) {
+                    self.circleImage.transform = CGAffineTransform(scaleX: 1, y: 1)
+                } completion: { (_) in
+                    if (self.continueAnimation){
+                        self.circleAnimation()
+                    }
+                }
+            }
+        }
+    }
 }
+
